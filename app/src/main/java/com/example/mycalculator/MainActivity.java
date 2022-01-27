c283a2a9a6c1254cc1bd3688ac73033459af50a9
Mikhail Zborovskiy <grinder1102@mail.ru>
2022-01-27 20:38:53 +0300

    Добавлена библиотека mxparser

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.mycalculator;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import org.mariuszgromada.math.mxparser.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
